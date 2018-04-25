81638613654a840273b8f903bcb172a6ddc0cf62
AnnaFlourou <flourou.anna@gmail.com>
2018-04-25 15:30:57 +0200

    add exo tdd typescript part3

--- src/params/default.spec.ts
+++ src/params/default.spec.ts
@@ -5,10 +5,10 @@ describe('default parameters', () => {
       return name
     }
 
-    expect(shoot('Chris')).toBe(/* YOUR ANSWER */)
-    expect(shoot(undefined)).toBe(/* YOUR ANSWER */)
-    expect(shoot(null)).toBe(/* YOUR ANSWER */)
-    expect(shoot()).toBe(/* YOUR ANSWER */)
+    expect(shoot('Chris')).toBe(/* YOUR ANSWER */ 'Chris')
+    expect(shoot(undefined)).toBe(/* YOUR ANSWER */ 'Steve')
+    expect(shoot(null)).toBe(/* YOUR ANSWER */null)
+    expect(shoot()).toBe(/* YOUR ANSWER */'Steve')
   })
 
   it('are not included in arguments', () => {
@@ -16,9 +16,9 @@ describe('default parameters', () => {
       return arguments.length
     }
 
-    expect(shoot('Chris')).toBe(/* YOUR ANSWER */)
-    expect(shoot(null)).toBe(/* YOUR ANSWER */)
-    expect(shoot()).toBe(/* YOUR ANSWER */)
+    expect(shoot('Chris')).toBe(/* YOUR ANSWER */1)
+    expect(shoot(null)).toBe(/* YOUR ANSWER */ 1)
+    expect(shoot()).toBe(/* YOUR ANSWER */ 0)
   })
 
   it('can trigger a function call', () => {
@@ -33,21 +33,22 @@ describe('default parameters', () => {
       return 'Steve'
     }
 
-    expect(triggerCount).toBe(/* YOUR ANSWER */)
-    expect(shoot('Chris')).toBe(/* YOUR ANSWER */)
-    expect(shoot()).toBe(/* YOUR ANSWER */)
-    expect(shoot(undefined)).toBe(/* YOUR ANSWER */)
-    expect(triggerCount).toBe(/* YOUR ANSWER */)
+    expect(triggerCount).toBe(/* YOUR ANSWER */ 0)
+    expect(shoot('Chris')).toBe(/* YOUR ANSWER */'Chris')
+    expect(shoot()).toBe(/* YOUR ANSWER */ 'Steve')
+    expect(shoot(undefined)).toBe(/* YOUR ANSWER */ 'Steve')
+    expect(triggerCount).toBe(/* YOUR ANSWER */2)
   })
 
   it('can default all arguments, optionally', () => {
     // Modify the method signature of `myFunction` to allow for all args to be optional
 
-    function myFunction({name, age, favoriteBand}) {
+    function myFunction({name = '', age = 0, favoriteBand = ''} = {}) {
       expect(name).toBeDefined()
       expect(age).toBeDefined()
       expect(favoriteBand).toBeDefined()
     }
+    
 
     myFunction({name: 'John', age: 40, favoriteBand: 'The Beatles'})
     myFunction({name: 'John', age: 40})
